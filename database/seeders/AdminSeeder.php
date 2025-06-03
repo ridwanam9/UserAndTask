c57bf1dfcd6ba4e5ac2a9adf498ec58a1809fb6d
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-06-03 21:09:09 +0700

    create admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'id' => Str::uuid(),
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'status' => true
+        ]);
+    }
+}
